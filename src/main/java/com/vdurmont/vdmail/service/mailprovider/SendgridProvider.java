a7f39053f188b1462aa7df7a45cf6a7b0aed20f8
Vincent DURMONT <vdurmont@gmail.com>
2015-03-29 18:07:45 -0700

    Implementation of the SengridProvider

--- src/main/java/com/vdurmont/vdmail/service/mailprovider/SendgridProvider.java
+++ src/main/java/com/vdurmont/vdmail/service/mailprovider/SendgridProvider.java
@@ -1,8 +1,10 @@
 package com.vdurmont.vdmail.service.mailprovider;
 
 import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
 import com.vdurmont.vdmail.exception.UnavailableProviderException;
 import com.vdurmont.vdmail.model.Email;
+import org.apache.log4j.Logger;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import static com.vdurmont.vdmail.tools.Strings.isNullOrEmpty;
 
 @Service
 public class SendgridProvider implements MailProvider {
+    private static final Logger LOGGER = Logger.getLogger(SendgridProvider.class);
+
     public static final String ENV_USERNAME = "sendgrid.username";
     public static final String ENV_PASSWORD = "sendgrid.password";
 
@@ -33,7 +37,28 @@ public class SendgridProvider implements MailProvider {
         if (this.sendGrid == null) {
             throw new UnavailableProviderException("Sendgrid was not properly configured.");
         }
-        // TODO code me
+        SendGrid.Email sendgridEmail = toSendgridEmail(email);
+
+        try {
+            SendGrid.Response response = sendGrid.send(sendgridEmail);
+            if (!response.getStatus()) {
+                LOGGER.warn("An error occurred while sending an email with Sendgrid: " + response.getMessage());
+                throw new UnavailableProviderException(response.getMessage());
+            }
+        } catch (SendGridException e) {
+            LOGGER.warn("An error occurred while sending an email with Sendgrid", e);
+            throw new UnavailableProviderException(e);
+        }
+    }
+
+    protected static SendGrid.Email toSendgridEmail(Email email) {
+        SendGrid.Email se = new SendGrid.Email();
+        se.setFromName(email.getSender().getName());
+        se.setFrom(email.getSender().getAddress());
+        se.addTo(email.getRecipient().getAddress());
+        se.setSubject(email.getSubject());
+        se.setText(email.getContent());
+        return se;
     }
 
     @Override public boolean isEnabled() {
